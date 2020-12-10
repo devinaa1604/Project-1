5b099e10a4a1c9b407352923d5d7658824b2cca4
tightknit <mr.armondbsmith@gmail.com>
2020-12-09 23:49:28 -0600

    Updated version of the readme.md

--- data_collection.py
+++ data_collection.py
@@ -1,1 +1,25 @@
-import requests
+Project 1 Outline
+
+![https://images.dwell.com/photos-6488407258118639616/6651031535077519360-large/a-fire-pit-between-two-wings-of-the-home-encourages-outdoor-living.jpg]()
+
+Where are they (maybe:home buyers) coming from and why and what does that mean for Texas Property Values?
+Real Estate Trends In Housing, Interest Rates, Relocation/Migration patterns for the last “Time Frame; TBD”
+What are the hot spots in Texas (top 5 cities):  El Paso, DFW, Houston, Austin, San Antonio.
+
+Where are they coming from (Top 5 States migrating to Texas)?  Is data readily available.  Buyer was located elsewhere, now lcoated in Texas
+What Demographics are making up these nomads (cowboys).  How demographics (age, income, family status) and motivations, etc. Influnence where buyer is relocating?
+
+Will our data validate the claim that our subjects are relocating based on a combination of factors but not limited to those highlighted?
+
+Factors to consider for relocation may/may not correlate to nomads moving to Texas’ Top 5 Cities:
+
+- Pollution rates/Env Data
+- Employment Data
+- Education / Property Tax Values -> Property prices
+- Mortgage Rate Data
+- Current Tax Climate in States leaving
+- Urban Centralization
+- Crime Rates
+- Corp Relocations
+- Weather
+
